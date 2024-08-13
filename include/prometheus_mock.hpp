254b5608a810b5df39ed3d69b1a690711b5e1869
Pedro Silva <pedrosilvamnchunks@gmail.com>
2024-08-13 21:19:34 +0200

    Done, but prometheus has a bug

--- include/prometheus_mock.hpp
+++ include/prometheus_mock.hpp
@@ -13,7 +13,7 @@ class prometheusMock : public prometheus
 public:
     prometheusMock() {}
     prometheusMock(std::string hostname, std::string port, std::string endpoint) : prometheus(hostname, port, endpoint) {}
-    MOCK_METHOD(http::response<http::string_body>, make_connection, (http::request<http::string_body> req,std::string & query), (override));
-    MOCK_METHOD(boost::json::object, make_query, (http::request<http::string_body> req,std::string & query), (override));
+    MOCK_METHOD(http::response<http::string_body>, make_connection, (http::request<http::string_body> req, std::string &query), (override));
+    MOCK_METHOD(boost::json::object, make_query, (http::request<http::string_body> req, std::string &query), (override));
 };
 #endif
